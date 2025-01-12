96ac611739a98f6b4816da6f66c271f922a12817
Shah Nawaz <shuvo1824@hotmail.com>
2025-01-12 23:29:22 +0600

    repl

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,16 @@ const std = @import("std");
 pub fn main() !void {
     // Uncomment this block to pass the first stage
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
+    while (true) {
+        try stdout.print("$ ", .{});
 
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    var input = std.mem.splitSequence(u8, user_input, " ");
+        var input = std.mem.splitSequence(u8, user_input, " ");
 
-    const command = input.next().?;
-    stdout.print("{s}: command not found\n", .{command}) catch {};
+        const command = input.next().?;
+        stdout.print("{s}: command not found\n", .{command}) catch {};
+    }
 }
