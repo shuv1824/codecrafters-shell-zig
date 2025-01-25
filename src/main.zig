8c755d5dcfcd1677f9be401637cae7f786985b48
Shah Nawaz <shuvo1824@hotmail.com>
2025-01-25 23:43:25 +0600

    the echo builtin

--- src/main.zig
+++ src/main.zig
@@ -13,11 +13,15 @@ pub fn main() !void {
         var input = std.mem.splitSequence(u8, user_input, " ");
 
         const command = input.next();
+        const args = input.rest();
 
         if (command) |c| {
             if (std.mem.eql(u8, c, "exit")) {
                 const exit_code = try std.fmt.parseInt(u8, input.next() orelse "0", 10);
                 std.process.exit(exit_code);
+            } else if (std.mem.eql(u8, c, "echo")) {
+                _ = try stdout.write(args);
+                _ = try stdout.write("\n");
             } else {
                 try stdout.print("{s}: command not found\n", .{c});
             }
